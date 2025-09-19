dca589cfa0743394a7fb0aa7a2b1bf33ba38fd62
lsdai1411 <lsydai6666@gmail.com>
2025-09-19 23:28:50 +0700

    Delete lab2mainstudent3.java

--- lab2mainstudent3.java
+++
@@ -1,9 +0,0 @@
-public class lab2mainstudent3 {
-    public static void main(String[] args) {
-    lab2student3 student = new lab2student3("sv01","Lesydai",2006,"Thanhhoa");
-        
-    System.out.println("Thong tin cua sinh vien la: ");
-    student.hienthisinhvien();
-    System.out.println("Hien nay sinh vien : "+ student.tinhtuoisinhvien());;
-    }
-}
